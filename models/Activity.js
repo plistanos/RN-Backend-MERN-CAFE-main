56ebaa241fbb90c2c19166dd9a2224198358ed0a
plistanos <michaelvilches747@gmail.com>
2021-11-21 23:48:57 -0300

    listo 6

--- models/Activity.js
+++ models/Activity.js
@@ -11,11 +11,17 @@ const ActivitySchema = Schema({
         default: true,
         required: true
     },
-    usuario: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Usuario',
-        required: true
-    }],
+    usuario: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Usuario',
+            required: true
+        },{
+            type: Schema.Types.ObjectId,
+            ref: 'Usuario',
+            required: true
+        }
+    ],
     precio: {
         type: Number,
         default: 0
