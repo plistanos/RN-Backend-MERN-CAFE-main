e232ab023775d05c122d77482cb108b1ac0c74de
plistanos <michaelvilches747@gmail.com>
2022-03-10 20:48:42 -0300

    nosex21

--- models/server.js
+++ models/server.js
@@ -37,12 +37,17 @@ class Server {
     }
 
     
+    corsOptions ={
+        origin:'*', 
+        credentials:true,            //access-control-allow-credentials:true
+        optionSuccessStatus:200,
+    }
 
 
     middlewares() {
 
         // CORS
-        this.app.use( cors() );
+        this.app.use(cors(this.corsOptions));
 
         
         // Lectura y parseo del body
