18e313ef9a89570288abc3ade556fe830d9e9c44
plistanos <michaelvilches747@gmail.com>
2022-03-16 17:27:53 -0300

    nosex31

--- middlewares/validar-archivo.js
+++ middlewares/validar-archivo.js
@@ -3,13 +3,13 @@ const { response } = require("express")
 
 const validarArchivoSubir = (req, res = response, next ) => {
 
-    // if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo ) {
-    //     return res.status(400).json({
-    //         msg: 'No hay archivos que subir - validarArchivoSubir'
-    //     });
-    // }
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo ) {
+        return res.status(400).json({
+            msg: 'No hay archivos que subir - validarArchivoSubir'
+        });
+    }
 
-    // next();
+    next();
 
 }
 
