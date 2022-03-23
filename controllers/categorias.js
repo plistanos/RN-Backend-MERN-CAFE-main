002f40e57e400ee052355171379cc845280c8cba
plistanos <michaelvilches747@gmail.com>
2022-03-23 00:34:26 -0300

    nosex38

--- controllers/categorias.js
+++ controllers/categorias.js
@@ -4,15 +4,15 @@ const { Categoria } = require('../models');
 
 const obtenerCategorias = async(req, res = response ) => {
 
-    const { limite = 5, desde = 0 } = req.query;
+    // const { limite = 5, desde = 0 } = req.query;
     const query = { estado: true };
 
     const [ total, categorias ] = await Promise.all([
         Categoria.countDocuments(query),
         Categoria.find(query)
             .populate('usuario', 'nombre')
-            .skip( Number( desde ) )
-            .limit(Number( limite ))
+            // .skip( Number( desde ) )
+            // .limit(Number( limite ))
     ]);
 
     res.json({
