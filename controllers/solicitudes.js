170dc92729c1e5e83fefca01f8661fe0c0c305ce
plistanos <michaelvilches747@gmail.com>
2022-03-23 22:17:36 -0300

    nosex40

--- controllers/solicitudes.js
+++ controllers/solicitudes.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const Solicitud = require('../models/Solicitud')
+const moment=require("moment")
 
 //Obtener Solicitudes
 const getSolicitudes = async(req, res = response) => {
@@ -44,7 +45,7 @@ const crearSolicitud = async(req, res = response) => {
     data.ticketsDisponibles = data.tickets;
     data.usuario = req.usuario._id;
     data.participantes=[];
-    data.fechaCreacion=momentStart.format('DD/MM/YYYY HH:mm');
+    data.fechaCreacion=moment().format('DD/MM/YYYY HH:mm');
 
     const solicitud = new Solicitud( data );
 
